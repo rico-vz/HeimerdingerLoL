dc4617c576a58b93d985667446304bd72cdbf1ca
Rico van Zelst <ricovzelst@pm.me>
2023-10-31 09:19:14 +0100

    fix: fix typo in skins database

--- database/seeders/ChampionSkinSeeder.php
+++ database/seeders/ChampionSkinSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Champion;
 use Illuminate\Database\Seeder;
 
 class ChampionSkinSeeder extends Seeder
@@ -11,6 +12,12 @@ class ChampionSkinSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $championDataUrl = "http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions.json";
+        $championData = json_decode(file_get_contents($championDataUrl), true);
+
+        foreach ($championData as $champion) {
+            $championId = $champion['id'];
+            $championExists = Champion::where('champion_id', $championId)->first();
+        }
     }
 }
