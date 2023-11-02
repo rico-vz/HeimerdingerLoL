18d293d04967bce0c74e6331ffa1a39c16dfb922
Rico van Zelst <ricovzelst@pm.me>
2023-11-02 14:59:15 +0100

    feat: scheduled data gathering

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('db:seed')->twiceDaily(1, 13)->timezone('Europe/Amsterdam');
     }
 
     /**
