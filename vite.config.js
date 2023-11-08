b0a88ba3b9c63a8bc345083c6469b94f2678cfac
Rico van Zelst <ricovzelst@pm.me>
2023-11-08 10:50:36 +0100

    Revert "feat: test css optim"
    This reverts commit f7272e015a2628974d09522285fcd0d68c3182a3.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import {optimizeCssModules} from "vite-plugin-optimize-css-modules";
 
 export default defineConfig({
     plugins: [
@@ -8,6 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        optimizeCssModules()
     ],
 });
