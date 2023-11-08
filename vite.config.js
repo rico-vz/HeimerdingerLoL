f7272e015a2628974d09522285fcd0d68c3182a3
Rico van Zelst <ricovzelst@pm.me>
2023-11-08 10:42:23 +0100

    feat: test css optim

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import {optimizeCssModules} from "vite-plugin-optimize-css-modules";
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        optimizeCssModules()
     ],
 });
