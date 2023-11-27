96c346f426077bd55a5765fbbe58840e225479f8
Rico van Zelst <ricovzelst@pm.me>
2023-11-27 17:47:01 +0100

    minor: add href.

--- resources/views/components/home/features.blade.php
+++ resources/views/components/home/features.blade.php
@@ -38,7 +38,7 @@
             </a>
 
             <a class="block p-8 transition bg-stone-800/40 border shadow-xl border-stone-800 rounded-xl hover:border-orange-500/10 hover:shadow-orange-500/10"
-               href="#">
+               href="/assets">
                 <x-iconsax-bul-3d-cube-scan class="h-10 text-orange-400"/>
 
                 <h2 class="mt-4 text-xl font-bold text-white">Game Assets</h2>
