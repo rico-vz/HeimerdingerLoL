2294140fcedbeae25351923fb17180b3958b2f17
Rico van Zelst <ricovzelst@pm.me>
2023-12-08 17:09:46 +0100

    refactor(views): Update home.blade.php and app.blade.php
    - Refactored the home.blade.php file to extend the layouts.app.blade.php file.
    - Moved the title and description sections to their respective yield sections in app.blade.php.
    - Removed duplicate code from home.blade.php.
    
    This commit improves code organization and removes duplication in the views.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,56 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'Heimerdinger.LoL • Home')
+@section('description', 'Explore League of Legends champions, skins, and game assets on Heimerdinger.LoL.
+Your ultimate source for in-depth information on LoL gaming. Dive in now!')
 
-    <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png">
-    <link rel="manifest" href="/img/icons/site.webmanifest">
-    <link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#e6855e">
-    <link rel="shortcut icon" href="/img/icons/favicon.ico">
-    <meta name="msapplication-TileColor" content="#ff7c47">
-    <meta name="msapplication-config" content="/img/icons/browserconfig.xml">
-    <meta name="theme-color" content="#ff7c47">
-
-    <title>Heimerdinger.LoL • Home</title>
-    <meta name="description"
-          content="Explore League of Legends champions, skins, and game assets on Heimerdinger.LoL. Your ultimate source for in-depth information on LoL gaming. Dive in now!">
-
-    <!-- OpenGraph -->
-    <meta property="og:site_name" content="Heimerdinger.LoL">
-    <meta property="og:title" content="Heimerdinger.LoL • Home">
-    <meta property="og:description" content="Explore League of Legends champions, skins, and game assets on Heimerdinger.LoL.
-          Your ultimate source for in-depth information on LoL gaming. Dive in now!">
-    <meta property="og:locale" content="en">
-    <meta property="og:type" content="website">
-    <meta property="og:image" content="{{asset('img/og_image.png')}}">
-
-    <!-- Twitter -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta property="twitter:domain" content="heimerdinger.lol">
-    <meta property="twitter:title" content="Heimerdinger.LoL • Home">
-    <meta property="twitter:description" content="Explore League of Legends champions, skins, and game assets on Heimerdinger.LoL.
-          Your ultimate source for in-depth information on LoL gaming. Dive in now!">
-    <meta property="twitter:image" content="{{asset('img/og_image.png')}}">
-
-    <link rel="preconnect" href="https://rsms.me/">
-    <link rel="preload" href="https://rsms.me/inter/inter.css" as="style">
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" media="print" onload="this.media='all'">
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body class="antialiased bg-stone-800 dark scroll-smooth">
-<x-navbar/>
-<x-home.features/>
-@if ($upcomingSkins != [])
-    <x-home.upcoming_skins :upcomingSkins="$upcomingSkins"/>
-@endif
-<x-home.recent_skins :latestSkins="$latestSkins"/>
-<x-footer/>
-</body>
-</html>
+@section('content')
+    <x-home.features/>
+    @if ($upcomingSkins != [])
+        <x-home.upcoming_skins :upcomingSkins="$upcomingSkins"/>
+    @endif
+    <x-home.recent_skins :latestSkins="$latestSkins"/>
+@endsection
