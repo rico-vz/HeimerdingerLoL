079cca375113b0b703232f7ea3abb52811937b9a
Rico van Zelst <ricovzelst@pm.me>
2023-12-12 14:41:49 +0100

    refactor: Remove unused imports and dependencies
    This commit removes unused imports and dependencies in the Listposts component, improving code cleanliness and reducing potential confusion.

--- app/View/Components/Posts/Listposts.php
+++ app/View/Components/Posts/Listposts.php
@@ -4,9 +4,7 @@ namespace App\View\Components\Posts;
 
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Collection;
 use Illuminate\View\Component;
-use Spatie\Sheets\Facades\Sheets;
 
 class Listposts extends Component
 {
