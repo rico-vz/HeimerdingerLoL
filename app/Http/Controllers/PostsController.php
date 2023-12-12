de94c1c2da715d04260dfcb190e719cb167da3a9
Rico <rico-vz@users.noreply.github.com>
2023-12-12 09:23:30 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,7 +10,7 @@ class PostsController extends Controller
     public function index()
     {
         $posts = Sheets::all();
-        
+
         return view('posts.index', compact('posts'));
     }
 
