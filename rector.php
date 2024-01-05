3717d3836e653fd813c0e7f9627c02637cfd7c38
Rico van Zelst <ricovzelst@pm.me>
2024-01-06 00:13:27 +0100

    feat(deps): add Rector for code quality improvements
    - Added "rector/rector" package to composer.json
    - Created rector.php configuration file with rules for code quality improvements

---
+++ rector.php
@@ -0,0 +1,22 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    // register single rule
+    $rectorConfig->rule(TypedPropertyFromStrictConstructorRector::class);
+
+    // here we can define, what sets of rules will be applied
+    // tip: use "SetList" class to autocomplete sets with your IDE
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::STRICT_BOOLEANS,
+        LevelSetList::UP_TO_PHP_82
+    ]);
+};
