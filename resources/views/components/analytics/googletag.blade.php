7826063f78cb9f9b2fb1717f4f49f370c0fb0d40
Rico van Zelst <ricovzelst@pm.me>
2024-02-26 13:38:51 +0100

    feat(analytics): replace old Google Tag script with new version
    - Added a new file googletag.blade.php for updated Google Tag script
    - Deleted the old version of googletag.blade.php containing outdated script

---
+++ resources/views/components/analytics/googletag.blade.php
@@ -0,0 +1,1 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('app.GTAG_MEASUREMENT_ID') }}"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '{{ config('app.GTAG_MEASUREMENT_ID') }}');</script>
