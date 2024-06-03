7a4b819641734331bbfced89cf0b39dc5eecfd96
Rico van Zelst <ricovzelst@pm.me>
2024-06-03 18:56:38 +0200

    style: update tailwind.config.js for consistency and new colors - Updated darkMode and content paths to use consistent quotes. - Added "text-violet-400" to the safelist. - Adjusted font size and family declarations for clarity. - Included new color variables for Twitch, YouTube, Kick, Douyu, Huya.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,47 +2,53 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    darkMode: 'class',
+    darkMode: "class",
     content: [
-        './resources/**/*.blade.php',
-        './resources/**/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-        './node_modules/flowbite/**/*.js',
+        "./resources/**/*.blade.php",
+        "./resources/**/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
     safelist: [
-        'text-stone-300',
-        'text-blue-400',
-        'text-red-500',
-        'text-pink-300',
-        'text-purple-500',
-        'text-yellow-400',
+        "text-stone-300",
+        "text-blue-400",
+        "text-red-500",
+        "text-pink-300",
+        "text-purple-500",
+        "text-yellow-400",
+        "text-violet-400",
     ],
     theme: {
         extend: {
             fontSize: {
-                '2xs': '.685rem',
+                "2xs": ".685rem",
             },
             fontFamily: {
-                sans: ['Inter var', 'Inter', 'sans-serif', ...defaultTheme.fontFamily.sans],
+                sans: [
+                    "Inter var",
+                    "Inter",
+                    "sans-serif",
+                    ...defaultTheme.fontFamily.sans,
+                ],
             },
             colors: {
-                twitch: '#6441a5',
-                youtube: '#FF0000',
-                kick: '#53fc18',
-                douyu: '#ff5f3a',
-                huya: '#ffaa06'
-            }
+                twitch: "#6441a5",
+                youtube: "#FF0000",
+                kick: "#53fc18",
+                douyu: "#ff5f3a",
+                huya: "#ffaa06",
+            },
         },
     },
     variants: {
         extend: {
-            textColor: ['group-hover'],
+            textColor: ["group-hover"],
         },
     },
     plugins: [
-        require('flowbite/plugin'),
-        require('tailwind-capitalize-first-letter'),
-        require('@tailwindcss/typography'),
+        require("flowbite/plugin"),
+        require("tailwind-capitalize-first-letter"),
+        require("@tailwindcss/typography"),
     ],
-}
+};
