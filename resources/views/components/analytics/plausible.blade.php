d0973f8f3ee6feec99dc7e1c41a8e3a4c6f10925
Rico van Zelst <rvzelst7@gmail.com>
2024-11-13 15:57:09 +0100

    fix: update plausible

--- resources/views/components/analytics/plausible.blade.php
+++ resources/views/components/analytics/plausible.blade.php
@@ -1,1 +1,2 @@
-<script defer data-domain="heimerdinger.lol" src="https://data.rico.sh/js/script.js"></script>
+<script defer data-domain="heimerdinger.lol" src="https://a.raining.win/js/script.outbound-links.js"></script>
+<script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
