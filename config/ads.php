76d468d1daa1883d45ae7c07d82c43fff920dd12
Rico van Zelst <rvzelst7@gmail.com>
2025-02-26 22:24:05 +0100

    feat: add AdSense configuration file
    - Introduced a new config file for AdSense settings.
    - Added client ID and ad slot identifiers for various placements.

---
+++ config/ads.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | AdSense Settings
+    |--------------------------------------------------------------------------
+    |
+    | This file contains the configuration for Google AdSense on Heimerdinger.lol
+    |
+    */
+
+    'client_id' => 'ca-pub-4505764048662657',
+
+    'slots' => [
+        'horizontal_banner' => '9840151408',
+        'vertical_banner' => '9117294343',
+        'in_article' => '7169787855',
+        'rectangle' => '1917461172',
+        'common' => '4128691547'
+    ]
+];
