22fbead9f4b038525f265dd5844f81975b849d38
Rico van Zelst <rvzelst7@gmail.com>
2025-02-26 22:25:26 +0100

    feat: add new ad components for various placements

---
+++ resources/views/components/ads/in-article.blade.php
@@ -0,0 +1,7 @@
+<div class="mx-auto my-6 text-center ad-container in-article-ad">
+    <ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid"
+        data-ad-client="{{ config('ads.client_id') }}" data-ad-slot="{{ config('ads.slots.in_article') }}"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
--- resources/views/components/ads/vertical-banner.blade.php
+++ resources/views/components/ads/vertical-banner.blade.php
@@ -1,6 +1,6 @@
-<div class="mx-auto my-6 text-center ad-container horizontal-banner">
+<div class="my-4 text-center ad-container vertical-banner">
     <ins class="adsbygoogle" style="display:block" data-ad-client="{{ config('ads.client_id') }}"
-        data-ad-slot="{{ config('ads.slots.horizontal_banner') }}" data-ad-format="auto"
+        data-ad-slot="{{ config('ads.slots.vertical_banner') }}" data-ad-format="auto"
         data-full-width-responsive="true"></ins>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
