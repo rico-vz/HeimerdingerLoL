35fde176f8d4d94e996ee1042d2619a98baa4786
Rico van Zelst <rvzelst7@gmail.com>
2025-03-18 10:11:48 +0100

    fix: update footer links
    Changed the creator link in the footer to a new URL. Updated the aria-label for better accessibility.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -51,7 +51,7 @@
         <span
             class="flex flex-row items-center justify-center text-sm text-gray-400 whitespace-pre sm:text-center dark:text-gray-400">© {{ date('Y') }} <a
                 href="/" class="hover:underline">Heimerdinger.LoL</a> • Made with <a
-                aria-label="Find out more about the creator of Heimerdinger.lol" href="https://rico.sh"><x-iconsax-bul-heart-circle
+                aria-label="Find out more about the creator of Heimerdinger.lol" href="https://solo.to/fwost"><x-iconsax-bul-heart-circle
                     class="relative self-center inline-block w-5 text-red-400"/></a></span>
 
         <span>   <p class="mt-3 font-mono text-sm text-gray-200 opacity-75">Version: {{ getCommitHash() }}</p> </span>
