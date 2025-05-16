99c8f9ee1773b39e2ced4f1dbbd441fbae1bacc6
Rico van Zelst <rvzelst7@gmail.com>
2025-05-16 14:15:01 +0200

    fix: remove unused splash_color

--- app/Http/Controllers/ChampionController.php
+++ app/Http/Controllers/ChampionController.php
@@ -15,9 +15,9 @@ class ChampionController extends Controller
     {
         $eightHoursInSeconds = 60 * 60 * 8;
 
-        $champions = Cache::remember('championsListAllCache', $eightHoursInSeconds, static fn () => Champion::orderBy('name')->get());
+        $champions = Cache::remember('championsListAllCache', $eightHoursInSeconds, static fn() => Champion::orderBy('name')->get());
 
-        $roles = Cache::remember('championsRolesCache', $eightHoursInSeconds, static fn () => ChampionRoles::orderBy('champion_name')->get());
+        $roles = Cache::remember('championsRolesCache', $eightHoursInSeconds, static fn() => ChampionRoles::orderBy('champion_name')->get());
 
         return view('champions.index', ['champions' => $champions, 'roles' => $roles]);
     }
@@ -28,19 +28,10 @@ class ChampionController extends Controller
     public function show(Champion $champion)
     {
         $threeDaysInSeconds = 60 * 60 * 24 * 3;
-        $sixMonthsInSeconds = 60 * 60 * 24 * 30 * 6;
 
-        $champion = Cache::remember('championShowCache'.$champion->slug, $threeDaysInSeconds, static fn () => $champion->load('streamers', 'skins', 'lanes'));
+        $champion = Cache::remember('championShowCache' . $champion->slug, $threeDaysInSeconds, static fn() => $champion->load('streamers', 'skins', 'lanes'));
 
         $streamers = $champion->load('streamers')->streamers;
-
-        $splashColor = Cache::remember(
-            'championSplashColorCache'.$champion->slug,
-            $sixMonthsInSeconds,
-            static fn () => getAverageColorFromImageUrl('https://wsrv.nl/?url='.$champion->getChampionImageAttribute(true))
-        );
-
-        $champion->splash_color = $splashColor;
 
         return view('champions.show', ['champion' => $champion, 'streamers' => $streamers]);
     }
