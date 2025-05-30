4850153b790131aa0f71550c56b04d12e4507a58
Rico van Zelst <ricovzelst@pm.me>
2025-05-30 23:20:42 +0200

    fix: update upcoming skins query
    update upcoming skins eloquent builder to fit the data the wiki started returning

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,11 +9,15 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $upcomingSkins = Cache::remember('upcomingSkins_home', 60 * 4, static fn () => ChampionSkin::where('availability', 'Upcoming')
-            ->where('release_date', '0000-00-00')
+        $upcomingSkins = Cache::remember('upcomingSkins_home', 60 * 4, static fn() => ChampionSkin::where('availability', 'Upcoming')
+            ->where(function ($query) {
+                $query->where('release_date', '0000-00-00')
+                    ->orWhere('release_date', '>', now());
+            })
             ->orderBy('release_date', 'desc')->get());
 
-        $latestSkins = Cache::remember('latestSkins_home', 60 * 4, static fn () => ChampionSkin::where('release_date', '!=', '0000-00-00')
+
+        $latestSkins = Cache::remember('latestSkins_home', 60 * 4, static fn() => ChampionSkin::where('release_date', '!=', '0000-00-00')
             ->orderBy('release_date', 'desc')->get());
 
         return view('home', [
