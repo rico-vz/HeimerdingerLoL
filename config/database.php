7651e99f26af36852158056179669ba065b0f418
Rico <rvzelst7@gmail.com>
2026-01-11 23:45:09 +0100

    feat: add MySQL connection configuration

--- config/database.php
+++ config/database.php
@@ -7,4 +7,14 @@ return [
         'update_date_on_publish' => false, // disable to preserve original behavior for existing applications
     ],
 
+    'connections' => [
+        'mysql' => [
+            'options' => extension_loaded('pdo_mysql') ? [
+                PDO::ATTR_TIMEOUT => 15,
+                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+            ] : [],
+            'sticky' => true,
+        ],
+    ],
+
 ];
